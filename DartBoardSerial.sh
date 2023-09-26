ef87969fa99cdbc979d4a28e9be32813cf65fe07
Jorge Alejandro Mejia <j.mejia2@utp.edu.co>
2023-09-26 18:03:49 -0500

    Bash v1 SerialEnd

---
+++ DartBoardSerial.sh
@@ -0,0 +1,16 @@
+#serial processing
+#!/bin/bash
+
+echo "Dart Board Serial test in progress..."
+echo "Number;Time" > DartBoardSerial.csv
+
+for i in {1..10}; do
+    echo "Running iteration $i"
+    for j in 100 1000 10000 100000 1000000 10000000 100000000; do
+        result=$(./DartBoardSerial $j)
+        echo "$j;$result" >> DartBoardSerial.csv
+    done
+done
+
+echo "" >> 'DartBoardSerial.csv'
+echo "done"
---
+++ MonteCarloNeedleSerial.sh
@@ -0,0 +1,16 @@
+#serial processing
+#!/bin/bash
+
+echo "Monte Carlo Needle Serial test in progress..."
+echo "Number;Time" > MonteCarloNeedleSerial.csv
+
+for i in {1..10}; do
+    echo "Running iteration $i"
+    for j in 100 1000 10000 100000 1000000 10000000 100000000; do
+        result=$(./MonteCarloNeedleSerial $j)
+        echo "$j;$result" >> MonteCarloNeedleSerial.csv
+    done
+done
+
+echo "" >> 'MonteCarloNeedleSerial.csv'
+echo "done"
