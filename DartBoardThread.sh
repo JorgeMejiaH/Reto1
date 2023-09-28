f90d1ab33028255797640bf14b5742b6ca34cfc1
Jorge Alejandro Mejia <j.mejia2@utp.edu.co>
2023-09-28 17:58:40 -0500

    Scrips

--- DartBoardThread.sh
+++ DartBoardThread.sh
@@ -1,14 +1,16 @@
 #serial processing
 #!/bin/bash
 
-echo "Dart Board Serial test in progress..."
-echo "Number;Time" > DartBoardThread.csv
+echo "Dart Board Thread test in progress..."
+echo "Number;Threads;Time" > DartBoardThread.csv
 
 for i in {1..10}; do
     echo "Running iteration $i"
-    for j in 100000 1000000 10000000 100000000 1000000000 10000000000 100000000000; do
-        result=$(./DartBoardThread $j)
-        echo "$j;$result" >> DartBoardThread.csv
+    for t in 2 4 8 16 32; do
+        for j in 100000 1000000 10000000 100000000 1000000000 10000000000 100000000000; do
+            result=$(./DartBoardThread $j $t)
+            echo "$j;$t;$result" >> DartBoardThread.csv
+        done
     done
 done
 
--- MonteCarloNeedleThread.sh
+++ MonteCarloNeedleThread.sh
@@ -1,14 +1,16 @@
 #serial processing
 #!/bin/bash
 
-echo "Monte Carlo Needle Serial test in progress..."
-echo "Number;Time" > MonteCarloNeedleThread.csv
+echo "Monte Carlo Needle Thread test in progress..."
+echo "Number;Threads;Time" > MonteCarloNeedleThread.csv
 
 for i in {1..10}; do
     echo "Running iteration $i"
-    for j in 100000 1000000 10000000 100000000 1000000000 10000000000 100000000000; do
-        result=$(./MonteCarloNeedleThread $j)
-        echo "$j;$result" >> MonteCarloNeedleThread.csv
+    for t in 2 4 8 16 32; do
+        for j in 100000 1000000 10000000 100000000 1000000000 10000000000 100000000000; do
+            result=$(./MonteCarloNeedleThread $j $t)
+            echo "$j;$t;$result" >> MonteCarloNeedleThread.csv
+        done
     done
 done
 
