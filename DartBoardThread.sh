58900f33e0fa4ad7d7cbcdf065ce8b80d3c2eb16
Jorge Alejandro Mejia <j.mejia2@utp.edu.co>
2023-09-28 12:01:58 -0500

    ThreadsV0.1.1

--- DartBoardThread.sh
+++ DartBoardThread.sh
@@ -2,15 +2,15 @@
 #!/bin/bash
 
 echo "Dart Board Serial test in progress..."
-echo "Number;Time" > DartBoardSerial.csv
+echo "Number;Time" > DartBoardThread.csv
 
 for i in {1..10}; do
     echo "Running iteration $i"
     for j in 100000 1000000 10000000 100000000 1000000000 10000000000 100000000000; do
-        result=$(./DartBoardSerial $j)
-        echo "$j;$result" >> DartBoardSerial.csv
+        result=$(./DartBoardThread $j)
+        echo "$j;$result" >> DartBoardThread.csv
     done
 done
 
-echo "" >> 'DartBoardSerial.csv'
+echo "" >> 'DartBoardThread.csv'
 echo "done"
--- MonteCarloNeedleThread.sh
+++ MonteCarloNeedleThread.sh
@@ -2,15 +2,15 @@
 #!/bin/bash
 
 echo "Monte Carlo Needle Serial test in progress..."
-echo "Number;Time" > MonteCarloNeedleSerial.csv
+echo "Number;Time" > MonteCarloNeedleThread.csv
 
 for i in {1..10}; do
     echo "Running iteration $i"
     for j in 100000 1000000 10000000 100000000 1000000000 10000000000 100000000000; do
-        result=$(./MonteCarloNeedleSerial $j)
-        echo "$j;$result" >> MonteCarloNeedleSerial.csv
+        result=$(./MonteCarloNeedleThread $j)
+        echo "$j;$result" >> MonteCarloNeedleThread.csv
     done
 done
 
-echo "" >> 'MonteCarloNeedleSerial.csv'
+echo "" >> 'MonteCarloNeedleThread.csv'
 echo "done"
