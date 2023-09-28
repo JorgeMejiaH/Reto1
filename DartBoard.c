24075bb72b7f4c9e4cfaf456273b6def65923a77
Jorge Alejandro Mejia <j.mejia2@utp.edu.co>
2023-09-28 17:22:58 -0500

    Thread DartBoard

--- DartBoard.c
+++ DartBoard.c
@@ -5,33 +5,68 @@
 #include <math.h>
 #include <pthread.h>
 
+typedef struct { //Estructura para definir los valores de cada hilo
+    double hits;
+    double total_throws;
+} ThreadResult;
+
+// Función que realiza el lanzamiento de dardos y cuenta los aciertos
+void *throw_darts(void *arg) {
+    ThreadResult *result = (ThreadResult *)arg;
+    const double factor = 1.0 / RAND_MAX;
+
+    for (double k = 0; k < result->total_throws; k++) {
+        double x_axis = rand() * factor;
+        double y_axis = rand() * factor;
+        if (x_axis * x_axis + y_axis * y_axis < 1) { // Dentro del círculo
+            result->hits++;
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
 int main(int argc, char *argv[]){
     struct timespec start, end;
     double elapsed_time;
-    double k, total_throws, hits;
+    double total_throws;
     const double factor = 1.0 / RAND_MAX;
     total_throws = atoi(argv[1]);
+    int Num_Threads = atoi(argv[2]);
     int verbose = 0;
 
-    if (argc > 2 && strcmp(argv[2], "-v") == 0) {//comando verbose
+    if (argc > 3 && strcmp(argv[3], "-v") == 0) {//comando verbose
         verbose = 1;
     }
 
     clock_gettime(CLOCK_MONOTONIC, &start); //Inicia Captura del tiempo
 
+    pthread_t threads[Num_Threads];
+    ThreadResult thread_results[Num_Threads];
+
     //initialize random generator
     srand((unsigned)time(NULL));
 
-    //Throw thosses
-    for(k=hits=0; k<total_throws; k++){
-        //Find two random numbers whithin 0..1
-        double x_axis = rand() * factor;
-        double y_axis = rand() * factor;
-        if(x_axis*x_axis + y_axis*y_axis < 1){ //Inside de Circle
-            hits++;
-        }
+    // Crear hilos y lanzar dardos
+    for (int i = 0; i < Num_Threads; i++) {
+        thread_results[i].hits = 0;
+        thread_results[i].total_throws = total_throws / Num_Threads;
+
+        pthread_create(&threads[i], NULL, throw_darts, (void *)&thread_results[i]);
     }
-    double Pi_aprox = 4.0 * hits / total_throws;
+
+    // Esperar a que los hilos terminen
+    for (int i = 0; i < Num_Threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    // Calcular la aproximación de π
+    double total_hits = 0;
+    for (int i = 0; i < Num_Threads; i++) {
+        total_hits += thread_results[i].hits;
+    }
+
+    double Pi_aprox = 4.0 * total_hits / total_throws;
 
     clock_gettime(CLOCK_MONOTONIC, &end);
     elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
