08c660dd2cddc7ff311acdc1beadd66f6cccd582
Jorge Alejandro Mejia <j.mejia2@utp.edu.co>
2023-10-02 23:22:17 -0500

    Data

--- MonteCarloNeedleFork.sh
+++ MonteCarloNeedleFork.sh
@@ -7,7 +7,7 @@ echo "Number;Process;Time" > MonteCarloNeedleFork.csv
 for i in {1..10}; do
     echo "Running iteration $i"
     for p in 2 4 8 16 32; do
-        for j in 100000 1000000 10000000 100000000 1000000000 10000000000 100000000000; do
+        for j in 100000 1000000 10000000 100000000 1000000000 10000000000; do
             result=$(./MonteCarloNeedleFork $j $p)
             echo "$j;$p;$result" >> MonteCarloNeedleFork.csv
         done
