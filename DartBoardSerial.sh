b1f4a9f588551f2dd47e4f27905cc87b5da4d2d0
Jorge Alejandro Mejia <j.mejia2@utp.edu.co>
2023-10-02 22:37:57 -0500

    Data

--- DartBoardSerial.sh
+++ DartBoardSerial.sh
@@ -6,7 +6,7 @@ echo "Number;Time" > DartBoardSerial.csv
 
 for i in {1..10}; do
     echo "Running iteration $i"
-    for j in 100000 1000000 10000000 100000000 1000000000 10000000000 100000000000; do
+    for j in 100000 1000000 10000000 100000000 1000000000 10000000000; do
         result=$(./DartBoardSerial $j)
         echo "$j;$result" >> DartBoardSerial.csv
     done
