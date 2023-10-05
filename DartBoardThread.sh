935d85d111c97fa055f81c744f643c828b07b869
Jorge Alejandro Mejia <j.mejia2@utp.edu.co>
2023-10-05 05:48:09 -0500

    bug

--- DartBoardThread.sh
+++ DartBoardThread.sh
@@ -7,7 +7,7 @@ echo "Number;Threads;Time" > DartBoardThread.csv
 for i in {1..10}; do
     echo "Running iteration $i"
     for t in 2 4 8 16 32; do
-        for j in 100000 1000000 10000000 100000000 1000000000 10000000000 100000000000; do
+        for j in 100000 1000000 10000000 100000000 1000000000 10000000000; do
             result=$(./DartBoardThread $j $t)
             echo "$j;$t;$result" >> DartBoardThread.csv
         done
